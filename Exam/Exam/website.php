6d932303954f9f99da78bea6448cc4d93e75c2e5
sedamiqaelyan <smiqaely1990>
2016-07-20 10:38:19 +0400

    second

--- Exam/Exam/website.php
+++ Exam/Exam/website.php
@@ -1,35 +1,48 @@
 <!Doctype html>
+<style>
+    img {
+        height: 100px;
+        width: 100px;
+    }
+</style>
+        <?php
+        require_once 'navigation.php';
+        $sql="SELECT
+            `news`.`title` as news_title,
+            `news`.`content` as news_content,
+            `news`.`date` as date,
+            `category`.`title` as news_category,
+            `news`.`news_image_name` as `news_image`
+            FROM
+            `news` INNER JOIN `category` ON `news`.categori_id=`category`.`id`";
+        $result = mysqli_query($dbConnection, $sql);
+        if (mysqli_num_rows($result) > 0) {
+            // output data of each row
+            while ($row = mysqli_fetch_assoc($result)) {
+                $news[] = $row;
+            }
+        }
+        ?>
+        <div class="media "style="padding-top: 50px;">
+            <div class="media-left media-middle">
+                <a href="#">
                 <?php
-                require_once 'navigation.php';
-                $sql="SELECT
-                    `news`.`title` as news_title,
-                    `news`.`content` as news_content,
-                    `news`.`date` as date,
-                    `category`.`title` as news_category
-                    FROM
-                    `news` INNER JOIN `category` ON `news`.categori_id=`category`.`id`";
-                $result = mysqli_query($dbConnection, $sql);
-                if (mysqli_num_rows($result) > 0) {
-                    // output data of each row
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        $news[] = $row;
-                    }
+                foreach ($news as $key=>$value) {
+                    echo '<img class="media-object" src=" ' . $value['news_image'] . '">';
+                    echo '<div class="border" style="height: 50px;"></div>';
                 }
                 ?>
-                <div class="media "style="padding-top: 50px;">
-                <div class="media-left media-middle">
-                    <a href="#">
-<!--                        <img class="media-object" src="images/.jpg" alt="...">-->
-                    </a>
-                </div>
-                <div class="media-body">
-                    <?php
-                    foreach ($news as $key=>$value) {
-                        echo '<h4 style="color: darkblue;">'.$value['news_title'].'</h4>'.PHP_EOL;
-                        echo $value['news_content'].PHP_EOL;
-                        echo $value['date'].PHP_EOL;
-                    }
-                    ?>
+                </a>
+            </div>
+            <div class="media-body">
+                <?php
+                foreach ($news as $key => $value){
+                    echo '<h4 style="color: darkblue;">'.$value['news_title'].'</h4>';
+                    echo $value['news_content'];
+                    echo $value['date'];
+                    echo '<div class="border" style="height: 50px;"></div>';
+                }
+            ?>
             </div>
         </div>
 <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
